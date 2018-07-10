f56107f960d633bd11389177cb103d44416b3ebe
kingrayhan <rayhan095@gmail.com>
2018-07-10 19:29:01 +0600

    again push

--- resources/assets/js/components/about-me.vue
+++ resources/assets/js/components/about-me.vue
@@ -2,8 +2,6 @@
 	<div class="about-me pt-5 pb-5">
 		<div class="container">
 			<div class="row">
-
-					
 					<div class="section-title col-md-4">
 						<h5 class="title">About Me</h5>
 						<h3 class="hash-sign">#</h3>
@@ -13,6 +11,7 @@
 						<p>I used to be learn coding. I love noting else coding. I code in php , java , python , c++ but I love javascript most. No girls love me , but I love to do code. Sometimes I thought if I could live in a deep forest where I have a computer and a internet connection and enough food. No one will disturb me, I can learn till my very last breath. :D</p>
 
 						<a href="https://www.linkedin.com/in/kingrayhan/" target="_blank" class="brack-btn">[ LinkedIn ]</a>
+						
 						<a href="https://www.upwork.com/freelancers/~01f5b2b716e00e089b" target="_blank" class="brack-btn">[ Upwork ]</a>
 					</div>
 			</div>
--- resources/assets/js/components/v-menu.vue
+++ resources/assets/js/components/v-menu.vue
@@ -8,11 +8,17 @@
 
 		<nav>
 			<a href="https://www.youtube.com/channel/UCxFDOHULIH3z-wDsR77-_0Q" target="_blank">Youtube</a>
+
 			<span class="line"></span>
+
 			<a href="https://github.com/kingRayhan" target="_blank">Github</a>
+
 			<span class="line"></span>
+
 			<a href="https://www.facebook.com/rayhan095" target="_blank">Facebook</a>
+
 			<span class="line"></span>
+			
 			<a href="https://medium.com/@kingrayhan" target="_blank">My Blog</a>
 		</nav>
 	</div>
