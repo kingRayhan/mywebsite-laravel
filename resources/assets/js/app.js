fc5722be1b51da68fca728b5aab26d7e4f6b6430
kingrayhan <rayhan095@gmail.com>
2018-07-13 12:36:02 +0600

    add instagram section

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,11 +1,11 @@
-require('./bootstrap');
+require('./bootstrap')
 window.Vue = require('vue')
 
-Vue.component('mywebsite', require('./components/mywebsite.vue'));
-Vue.component('hero', require('./components/hero.vue'));
-Vue.component('about-me', require('./components/about-me.vue'));
-Vue.component('v-menu', require('./components/v-menu.vue'));
-Vue.component('instagram-gallery', require('./components/instagram-gallery.vue'));
+Vue.component('mywebsite', require('./components/mywebsite.vue'))
+Vue.component('hero', require('./components/hero.vue'))
+Vue.component('about-me', require('./components/about-me.vue'))
+Vue.component('v-menu', require('./components/v-menu.vue'))
+Vue.component('instagram-gallery', require('./components/instagram-gallery.vue'))
 
 import router from './router' // routes
 
--- resources/assets/js/components/instagram-gallery.vue
+++ resources/assets/js/components/instagram-gallery.vue
@@ -1,17 +1,32 @@
 <template>
-	<div>
-		<h1>Instagram</h1>
+	<div class="pt-5">
+		<Carousel :autoplay="true" :loop="true" :perPageCustom="[[320, 2], [768, 3], [769, 5]]">
+			<Slide v-for="image in images" :key="image.id">
+				<img :src="image.images.standard_resolution.url">
+			</Slide>
+		</Carousel>
 	</div>
 </template>
-
 <script>
+	import { Carousel, Slide } from 'vue-carousel'
 	export default{
+		components: {
+		    Carousel,
+		    Slide
+		},
 		created()
 		{
 			const api = `https://api.instagram.com/v1/users/self/media/recent/?access_token=2286603943.7a4623b.d9d4b1b436734bc293c21df23ef4f8d2`
 			
-			axios.get(api)
-			.then( ({data}) => console.log(data) )			
+			$.ajax({
+			        method: "GET",
+			        url: api,
+			        dataType: "jsonp",
+			        jsonp: "callback",
+			        jsonpCallback: "jsonpcallback",
+			        success: ({ data }) => this.images = data
+			        // success: ({ data }) => console.log(data)
+			    });		
 		},
 		data()
 		{
